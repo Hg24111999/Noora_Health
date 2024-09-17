3d1817a4d099561180e64dd6de4b20576b08ef2c
Harsh Gupta <guptaharsh784@gmail.com>
2024-09-17 23:14:07 +0530

    fixing error

--- models/Messages_transformed.sql
+++ models/Messages_transformed.sql
@@ -3,37 +3,38 @@ WITH status_ranked AS (
         "message_id",
         "status",
         "timestamp",
-        ROW_NUMBER() OVER (PARTITION BY "message_id" ORDER BY "timestamp" DESC) AS "rn"
-    FROM {{ source('public', 'Noora_Statuses') }}
-),
+        ROW_NUMBER() OVER (PARTITION BY message_id ORDER BY timestamp DESC) AS "rn"
+    FROM public."Noora_Statuses"
+)
+,
 status_pivot AS (
     SELECT
         "message_id",
-        MAX(CASE WHEN "rn" = 1 THEN "status" END) AS "status_1",
-        MAX(CASE WHEN "rn" = 1 THEN "timestamp" END) AS "timestamp_status_1",
-        MAX(CASE WHEN "rn" = 2 THEN "status" END) AS "status_2",
-        MAX(CASE WHEN "rn" = 2 THEN "timestamp" END) AS "timestamp_status_2",
-        MAX(CASE WHEN "rn" = 3 THEN "status" END) AS "status_3",
-        MAX(CASE WHEN "rn" = 3 THEN "timestamp" END) AS "timestamp_status_3",
-        MAX(CASE WHEN "rn" = 4 THEN "status" END) AS "status_4",
-        MAX(CASE WHEN "rn" = 4 THEN "timestamp" END) AS "timestamp_status_4",
-        MAX(CASE WHEN "rn" = 5 THEN "status" END) AS "status_5",
-        MAX(CASE WHEN "rn" = 5 THEN "timestamp" END) AS "timestamp_status_5",
-        MAX(CASE WHEN "rn" = 6 THEN "status" END) AS "status_6",
-        MAX(CASE WHEN "rn" = 6 THEN "timestamp" END) AS "timestamp_status_6",
-        MAX(CASE WHEN "rn" = 7 THEN "status" END) AS "status_7",
-        MAX(CASE WHEN "rn" = 7 THEN "timestamp" END) AS "timestamp_status_7",
-        MAX(CASE WHEN "rn" = 8 THEN "status" END) AS "status_8",
-        MAX(CASE WHEN "rn" = 8 THEN "timestamp" END) AS "timestamp_status_8",
-        MAX(CASE WHEN "rn" = 9 THEN "status" END) AS "status_9",
-        MAX(CASE WHEN "rn" = 9 THEN "timestamp" END) AS "timestamp_status_9",
-        MAX(CASE WHEN "rn" = 10 THEN "status" END) AS "status_10",
-        MAX(CASE WHEN "rn" = 10 THEN "timestamp" END) AS "timestamp_status_10"
+         MAX(CASE WHEN rn = 1 THEN status END) AS "status_1",
+         MAX(CASE WHEN rn = 1 THEN timestamp END) AS "timestamp_status_1",
+         MAX(CASE WHEN rn = 2 THEN status END) AS "status_2",
+         MAX(CASE WHEN rn = 2 THEN timestamp END) AS "timestamp_status_2",
+         MAX(CASE WHEN rn = 3 THEN status END) AS "status_3",
+         MAX(CASE WHEN rn = 3 THEN timestamp END) AS "timestamp_status_3",
+         MAX(CASE WHEN rn = 4 THEN status END) AS "status_4",
+         MAX(CASE WHEN rn = 4 THEN timestamp END) AS "timestamp_status_4",
+         MAX(CASE WHEN rn = 5 THEN status END) AS "status_5",
+         MAX(CASE WHEN rn = 5 THEN timestamp END) AS "timestamp_status_5",
+		 MAX(CASE WHEN rn = 6 THEN status END) AS "status_6",
+         MAX(CASE WHEN rn = 6 THEN timestamp END) AS "timestamp_status_6",
+         MAX(CASE WHEN rn = 7 THEN status END) AS "status_7",
+         MAX(CASE WHEN rn = 7 THEN timestamp END) AS "timestamp_status_7",
+         MAX(CASE WHEN rn = 8 THEN status END) AS "status_8",
+         MAX(CASE WHEN rn = 8 THEN timestamp END) AS "timestamp_status_8",
+         MAX(CASE WHEN rn = 9 THEN status END) AS "status_9",
+         MAX(CASE WHEN rn = 9 THEN timestamp END) AS "timestamp_status_9",
+         MAX(CASE WHEN rn = 10 THEN status END) AS "status_10",
+         MAX(CASE WHEN rn = 10 THEN timestamp END) AS "timestamp_status_10"
     FROM status_ranked
-    GROUP BY "message_id"
+    GROUP BY message_id
 )
 SELECT
-    m."id" AS "message_id",
+    m."id" as "message_id",
     m."content",
     m."message_type",
     m."masked_addressees",
@@ -52,7 +53,7 @@ SELECT
     sp."timestamp_status_4",
     sp."status_5",
     sp."timestamp_status_5",
-    sp."status_6",
+	sp."status_6",
     sp."timestamp_status_6",
     sp."status_7",
     sp."timestamp_status_7",
@@ -62,10 +63,10 @@ SELECT
     sp."timestamp_status_9",
     sp."status_10",
     sp."timestamp_status_10",
-    sp."message_id" AS "status_message_id",
+    sp."message_id" as "status_message_id",
     m."rendered_content",
     m."uuid",
     m."inserted_at",
     m."updated_at"
-FROM {{ source('public', 'Noora_Messages') }} AS m
-LEFT JOIN status_pivot sp ON m."id" = sp."message_id"
+FROM public."Noora_Messages" m
+LEFT JOIN status_pivot sp ON m.id=sp.message_id;
