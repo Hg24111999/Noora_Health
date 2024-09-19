6b59a2f20afbdbd28defecfb0fb3f0e3478e0651
Harsh Gupta <82459031+Hg24111999@users.noreply.github.com>
2024-09-19 13:33:36 +0530

    Update Messages_transformed.sql

--- models/Messages_transformed.sql
+++ models/Messages_transformed.sql
@@ -4,7 +4,7 @@ WITH status_ranked AS (
         "status",
         "timestamp",
         ROW_NUMBER() OVER (PARTITION BY message_id ORDER BY timestamp DESC) AS "rn"
-    FROM erp_db.public.Noora_Statuses
+    FROM airbyte_internal.public_raw__stream_noora_statuses
 )
 ,
 status_pivot AS (
@@ -68,5 +68,5 @@ SELECT
     m."uuid",
     m."inserted_at",
     m."updated_at"
-FROM erp_db.public.Noora_Messages as  m
+FROM airbyte_internal.public_raw__stream_noora_messages as  m
 LEFT JOIN status_pivot sp ON m.id=sp.message_id
